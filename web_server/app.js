d9653c0d2f7f4f2ec107b2c1b51349aa7735b748
1legality <markis.landry@gmail.com>
2017-02-28 15:02:48 -0500

    add index.htm and use fs instead of magic variable

--- web_server/app.js
+++ web_server/app.js
@@ -1,9 +1,12 @@
 var http = require('http');
+var fs = require('fs');
 
 http.createServer(function(req, res) {
     res.writeHead(200, {
-        'Content-Type': 'text/plain' 
+        'Content-Type': 'text/html' 
     });
 
-    res.end('Hello world\n');
+    var html = fs.readFileSync(__dirname + '/index.htm');
+
+    res.end(html);
 }).listen(1337, '127.0.0.1');
