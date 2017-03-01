cb27aefe718f00166c0a3c51ceec0b5b93587f12
1legality <markis.landry@gmail.com>
2017-03-01 15:49:08 -0500

    Change "dans" for "and"

--- web_server_express/index.js
+++ web_server_express/index.js
@@ -4,7 +4,7 @@ var app = express();
 // Use env variable if exists, else use port 3000
 var port = process.env.PORT | 3000;
 
-// Middleware, everytime we see /assets, convert is to /public dans then loads it.
+// Middleware, everytime we see /assets, convert is to /public and then loads it.
 app.use('/assets', express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
